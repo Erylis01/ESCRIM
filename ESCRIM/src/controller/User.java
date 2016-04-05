73ad6de1695beffc95aad2faff70fff7e7b2c778
Pierre C <pierre.coquerel@mines-ales.org>
2016-04-05 10:15:29 +0200

    Generation des getters et setters

--- ESCRIM/src/controller/User.java
+++ ESCRIM/src/controller/User.java
@@ -2,6 +2,62 @@ package controller;
 
 public class User {
 
-	private String nom,prenom,identifiant,mdp;
+	private String lastname,firstname,login,password;
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
