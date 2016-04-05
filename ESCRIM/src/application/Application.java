dc675b7bb0900ae2559c57432fd756c0d86e7240
Pierre C <pierre.coquerel@mines-ales.org>
2016-04-05 16:55:42 +0200

    Ajout de methode static dans Aplication pour pas changer le main de tout le monde à chaque fois

--- ESCRIM/src/application/Application.java
+++ ESCRIM/src/application/Application.java
@@ -1,10 +1,25 @@
 package application;
 
+
+
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PierreTests();
+		CharlieTests();
+		WilliamTests();
+	}
+
+	private static void PierreTests() {
 
 	}
 
+	private static void CharlieTests() {
+
+	}
+	
+	private static void WilliamTests() {
+
+	}
 }
--- ESCRIM/src/gui/Interface.java
+++ ESCRIM/src/gui/Interface.java
@@ -1,5 +1,30 @@
 package gui;
 
-public class Interface {
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+/**
+ * La Classe Interface représente la JFrame générale du logiciel.
+ * 
+ * @author Pierre
+ */
+public class Interface extends JFrame {
+	
+	
+
+	/**
+	 * Permet de créer une fenetre;
+	 */
+
+	public void createwindow() {
+
+		
+		// Donne le titre de la fenêtre
+		this.setTitle("ESCRIM");
+	}
+	
+	
+	
 }
