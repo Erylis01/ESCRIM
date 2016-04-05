6e7c2c0f1d2ed7b26ddc6aedf801d1e5ac7103d0
Pierre C <pierre.coquerel@mines-ales.org>
2016-04-05 10:12:14 +0200

    Ajout des attributs des classes du package "model".

--- ESCRIM/src/model/Product.java
+++ ESCRIM/src/model/Product.java
@@ -4,5 +4,105 @@ import java.sql.Date;
 
 public class Product {
 
+	private Date dlu;
+	private int quantity,lot,critical_threshold;
+	private String dcl,dosage,reference,name;
+	
+	/**
+	 * @return the dlu
+	 */
+	public Date getDlu() {
+		return dlu;
+	}
+	/**
+	 * @param dlu the dlu to set
+	 */
+	public void setDlu(Date dlu) {
+		this.dlu = dlu;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	/**
+	 * @return the lot
+	 */
+	public int getLot() {
+		return lot;
+	}
+	/**
+	 * @param lot the lot to set
+	 */
+	public void setLot(int lot) {
+		this.lot = lot;
+	}
+	/**
+	 * @return the critical_threshold
+	 */
+	public int getCritical_threshold() {
+		return critical_threshold;
+	}
+	/**
+	 * @param critical_threshold the critical_threshold to set
+	 */
+	public void setCritical_threshold(int critical_threshold) {
+		this.critical_threshold = critical_threshold;
+	}
+	/**
+	 * @return the dcl
+	 */
+	public String getDcl() {
+		return dcl;
+	}
+	/**
+	 * @param dcl the dcl to set
+	 */
+	public void setDcl(String dcl) {
+		this.dcl = dcl;
+	}
+	/**
+	 * @return the dosage
+	 */
+	public String getDosage() {
+		return dosage;
+	}
+	/**
+	 * @param dosage the dosage to set
+	 */
+	public void setDosage(String dosage) {
+		this.dosage = dosage;
+	}
+	/**
+	 * @return the reference
+	 */
+	public String getReference() {
+		return reference;
+	}
+	/**
+	 * @param reference the reference to set
+	 */
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
--- ESCRIM/src/model/Storage.java
+++ ESCRIM/src/model/Storage.java
@@ -1,5 +1,107 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Storage {
 
+	private int number,width,height,depth;
+	private String type,localisation,state;
+	private ArrayList<Product> list_product = new  ArrayList<Product>();
+	
+	/**
+	 * @return the number
+	 */
+	public int getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	/**
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	/**
+	 * @return the depth
+	 */
+	public int getDepth() {
+		return depth;
+	}
+	/**
+	 * @param depth the depth to set
+	 */
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the localisation
+	 */
+	public String getLocalisation() {
+		return localisation;
+	}
+	/**
+	 * @param localisation the localisation to set
+	 */
+	public void setLocalisation(String localisation) {
+		this.localisation = localisation;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the list_product
+	 */
+	public ArrayList<Product> getList_product() {
+		return list_product;
+	}
+	/**
+	 * @param list_product the list_product to set
+	 */
+	public void setList_product(ArrayList<Product> list_product) {
+		this.list_product = list_product;
+	}
 }
