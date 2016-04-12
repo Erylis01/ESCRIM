f2094a8a1b92f7ffa87e2cfd9c57e4117247fec5
Charlie <Charlie@159.31.249.165>
2016-04-12 17:27:52 +0200

    RecupInfosProduit

--- ESCRIM/src/controller/Bdd.java
+++ ESCRIM/src/controller/Bdd.java
@@ -3,9 +3,10 @@ package controller;
 import java.sql.*;
 
 public class Bdd {
-
-	public static void ConnectDB(){
+	
+	public static Connection ConnectDB(){
 		
+		Connection conn = null;
 		//Récupérer le Driver
 		try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -15,8 +16,10 @@ public class Bdd {
         	System.out.println("erreur");
 		}
 		
-		Connection conn = null;
+		
 		try {
+			
+			
 		    conn = DriverManager.getConnection("jdbc:mysql://51.254.124.54:3306/ESCRIM_BDD","ESCRIM","w859uxZrvpBwb4Vt");
 		    System.out.println("Okk !");
 
@@ -27,9 +30,57 @@ public class Bdd {
 		    System.out.println("SQLState: " + ex.getSQLState());
 		    System.out.println("VendorError: " + ex.getErrorCode());
 		}
+		return conn;
 	}
 	
-	public static void Authenticate(){
+	public static boolean Authenticate(String username, String password, Connection conn) throws SQLException{
+		boolean authenticated = true;
+		Statement stmt = null;
 		
+		System.out.println("Creating statement...");
+	    stmt = conn.createStatement();
+	    String sql;
+	    sql = "SELECT Username, Password FROM Users";
+	    ResultSet rs = stmt.executeQuery(sql);
+	    while (rs.next()){
+	    	String ndc=rs.getString("Username");
+	    	String mdp=rs.getString("Password");
+	    	
+	    	if (ndc==username && mdp==password){
+	    		authenticated=true;
+	    	}
+	    	
+	    }
+		return authenticated;
+	}
+	public static String[] RecupInfoProduit(int Lot, Connection conn) throws SQLException{
+		String[] Infos = new String[9];
+		Statement stmt=null;
+		stmt=conn.createStatement();
+		
+		String sql;
+		sql="SELECT Id, designation, dlu, reference, quantite, dosage, lot, dci, seuil_critique FROM Produits WHERE lot="+Lot;
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		int id=rs.getInt("Id");
+		String designation=rs.getString("designation");
+		Date dlu=rs.getDate("dlu");
+		String reference=rs.getString("reference");
+		int quantite=rs.getInt("quantite");
+		int dosage=rs.getInt("dosage");
+		Date dci=rs.getDate("dci");
+		int seuil_critique=rs.getInt("seuil_critique");
+		
+		Infos[0]=String.valueOf(id);
+		Infos[1]=designation;
+		Infos[2]=String.valueOf(dlu);
+		Infos[3]=reference;
+		Infos[4]=String.valueOf(quantite);
+		Infos[5]=String.valueOf(dosage);
+		Infos[6]=String.valueOf(Lot);
+		Infos[7]=String.valueOf(dci);
+		Infos[8]=String.valueOf(seuil_critique);
+		
+		return Infos;
 	}
 }
