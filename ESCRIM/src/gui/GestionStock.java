58a6a73f1f82039e9377dcf3c3a4480c5a0b8646
William <William@159.31.250.31>
2016-04-17 21:58:13 +0200

    Modif fenetre stock

--- ESCRIM/src/gui/GestionStock.java
+++ ESCRIM/src/gui/GestionStock.java
@@ -20,8 +20,13 @@ import javax.swing.UIManager;
 import javax.swing.AbstractListModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import java.awt.TextField;
+import javax.swing.JButton;
 
 public class GestionStock extends JFrame {
+	private JTable table;
 	public GestionStock() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(GestionStock.class.getResource("/assets/icone.png")));
 		setVisible(true);
@@ -54,19 +59,55 @@ public class GestionStock extends JFrame {
 		label_2.setBounds(60, 36, 49, 14);
 		panel_user.add(label_2);
 		
+		JButton btnAdd = new JButton("Ajouter");
+		btnAdd.setBounds(108, 459, 139, 23);
+		panel.add(btnAdd);
+		
+		JLabel lblQuantit = new JLabel("Quantit\u00E9 :");
+		lblQuantit.setHorizontalAlignment(SwingConstants.CENTER);
+		lblQuantit.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblQuantit.setBounds(26, 428, 77, 14);
+		panel.add(lblQuantit);
+		
+		JLabel lblNDeLot = new JLabel("N\u00B0 de lot :");
+		lblNDeLot.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNDeLot.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNDeLot.setBounds(26, 405, 77, 14);
+		panel.add(lblNDeLot);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(109, 403, 138, 20);
+		panel.add(comboBox_1);
+		
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setToolTipText("");
+		table.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.setBounds(26, 373, 529, -279);
+		panel.add(table);
+		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		comboBox.setMaximumRowCount(60);
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"-1"}));
 		comboBox.setSelectedIndex(0);
-		comboBox.setBounds(298, 37, 123, 20);
+		comboBox.setBounds(304, 37, 123, 20);
 		panel.add(comboBox);
 		
 		JLabel lblNDeStockage = new JLabel("N\u00B0 de stockage :");
 		lblNDeStockage.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNDeStockage.setBounds(165, 40, 106, 14);
+		lblNDeStockage.setBounds(171, 40, 106, 14);
 		panel.add(lblNDeStockage);
 		lblNDeStockage.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		TextField quantiteAAjouter = new TextField();
+		quantiteAAjouter.setBounds(109, 428, 138, 22);
+		panel.add(quantiteAAjouter);
+		
+		JButton delSelect = new JButton("Supprimer");
+		delSelect.setBounds(354, 459, 123, 23);
+		panel.add(delSelect);
 		
 		JLabel lblFond = new JLabel("New label");
 		lblFond.setIcon(new ImageIcon(GestionStock.class.getResource("/assets/fond_logpan.jpg")));
